c9b6966554603cf210c3b1f419fe14f4118d2e1b
johnlee96 <johnleejiunherng@gmail.com>
2023-03-22 17:09:08 +0800

    test jinja template macro

--- dags/dags_with_no_tag.py
+++ dags/dags_with_no_tag.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from airflow.utils.task_group import TaskGroup
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.bash import BashOperator
 
 with DAG(dag_id="test_dag_with_no_tags", default_args={"start_date": datetime(2023, 3, 21)}, schedule_interval='@once') as dag:
     with TaskGroup('TaskGroupA') as TaskGroupA:
@@ -9,5 +10,6 @@ with DAG(dag_id="test_dag_with_no_tags", default_args={"start_date": datetime(20
             DummyOperator(task_id=f"test_task_a{i}")
     with TaskGroup('TaskGroupB') as TaskGroupB:
         for i in range(3):
-            DummyOperator(task_id=f"test_task_b{i}")
+            dateleft='{{macros.ds_add(ds,'+f'{i+1}'+')}}'
+            BashOperator(task_id=f"test_task_b{i}",bash_command=f'echo {dateleft}')
     TaskGroupA >> TaskGroupB
